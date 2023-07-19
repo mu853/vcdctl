4e0a445cb3610cca552201b9b6a65f3a8f67b265
mu853 <mu.youichi853@gmail.com>
2023-07-19 16:24:23 +0900

    fix post request

--- module/post.go
+++ module/post.go
@@ -1,20 +1,24 @@
 package module
 
 import (
-	"encoding/xml"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 func NewCmdPost() *cobra.Command {
 	var fileName string
+	var header string
 	cmd := &cobra.Command{
 		Use:   "post",
 		Short: "exec post api",
 		Args:  cobra.ExactArgs(1),
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return client.Login()
+		},
 		Run: func(cmd *cobra.Command, args []string) {
 			api := args[0]
 
@@ -22,20 +26,24 @@ func NewCmdPost() *cobra.Command {
 			if fileName != "" {
 				data = ReadRequestData(fileName)
 			}
-			res := client.Request("POST", api, nil, data)
+
+			var header_map map[string]string
+			if header != "" {
+				header_map = map[string]string{}
+				entry := strings.Split(header, ": ")
+				header_map[entry[0]] = entry[1]
+			}
+			res := client.Request("POST", api, header_map, data)
 			fmt.Println(string(res.Body))
 		},
 	}
 	cmd.Flags().StringVarP(&fileName, "filename", "f", "", "file name for send data(xml)")
+	cmd.Flags().StringVarP(&header, "header", "", "", "additional header (cf. \"Content-Type: application/vnd.vmware.vcloud.vm+xml\"")
 	return cmd
 }
 
 func ReadRequestData(fileName string) []byte {
-	raw_data, err := os.ReadFile(fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	data, err := xml.Marshal(raw_data)
+	data, err := os.ReadFile(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
